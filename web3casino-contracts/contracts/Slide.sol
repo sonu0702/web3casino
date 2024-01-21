1cd1224d8c2587bc6428582861fa16d1d78ca088
sonu-knight <sonu@metasky.ai>
2024-01-21 21:23:48 +0530

    init slide

---
+++ web3casino-contracts/contracts/Slide.sol
@@ -0,0 +1,4 @@
+//work on this
+//https://testnet.snowtrace.io/address/0xa3A3770c7c20FDD4910721268b08f9325657a33f/contract/43113/code
+// getPayout function
+// game less check is also in playGame() function along with validateBet function
